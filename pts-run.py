56ce9c8d6f117d7c9a6868c779c43f48d5d0e35c
Johnathan Becker <historybuffjb@gmail.com>
2018-11-23 14:44:59 -0700

    Update Code

--- pts-run.py
+++ pts-run.py
@@ -8,7 +8,7 @@ if(len(sys.argv)) != 2:
 
 
 class PhoronixTestSuite:
-    def __init__(self):
+    def __init__(self, folder_path):
         self._tests = ['blogbench', 'c-ray', 'cachebench', 'dacapobench', 'dolfyn', 'glibc-bench',
                        'himeno', 'hmmer', 'hpcg', 'iperf', 'lammps', 'm-queens', 'mcperf',
                        'mrbayes', 'namd', 'netperf', 'osbench', 'pjdfstest', 'polybench-c',
@@ -16,20 +16,19 @@ class PhoronixTestSuite:
                        'sysbench', 'systemd-boot-kernel', 'systemd-boot-total', 'systemd-boot-userspace-1.0.1',
                        'tachyon']
         self._magic = str(datetime.datetime.now()).replace(' ', '').replace(':', '').replace('.', '').replace('-', '')
-        self._git_name = 'https://github.com/ucd-graduate-os-final/Phoronix-Test-Suite'
-        self._git_folder = 'Phoronix-Git-Folder'
+        self._git_name = os.path.join('https:', 'github.com', 'ucd-graduate-os-final', 'Phoronix-Test-Suite')
+        self._git_folder = folder_path
+        self._test_suite_path = os.path.join('~', '.phoronix-test-suite', 'test-results')
 
     def _create_result_name(self, file_name):
         return '{}_{}'.format(file_name, self._magic)
 
     def run_tests(self):
         for test in self._tests:
-            file_name = self._create_result_name(test)
-            file_path = os.path.join('~/phoronix-test-suite', 'test_results', file_name)
-            os.system('phoronix-test-suite batch-run {} {}'.format(test, file_path))
-            self._move_file(file_path, os.path.join('~/{}'.format(self._git_folder), 'test_results', self._magic,
-                                                    file_name))
+            os.system('phoronix-test-suite result-file-to-json batch-run {}'.format(test))
         print("All tests completed. ")
+        self._move_folder_contents(self._test_suite_path, os.path.join(self._git_folder, self._magic))
+        self._remove_folder_contents(self._test_suite_path)
         self._push_to_git()
         self._stop_vm_instance()
 
@@ -40,21 +39,29 @@ class PhoronixTestSuite:
     def _stop_vm_instance(self):
         os.system('sudo shutdown -h now')
 
-    def _move_file(self, from_path, to_path):
+    def _move_folder_contents(self, from_path, to_path):
         try:
-            shutil.move(from_path, to_path)
+            shutil.copytree(from_path, to_path)
         except OSError:
-            print("{} failed to be moved to {}. Do this manually.".format(from_path, to_path))
+            print("{} failed to be moved to {}. Do thhis manually.".format(from_path, to_path))
         except Exception:
             raise Exception("Unknown error occurred. Exiting...")
 
+    def _remove_folder_contents(self, path):
+        try:
+            shutil.rmtree(path)
+        except OSError:
+            print("{} failed to be deleted. Do this manually".format(path))
+        except Exception:
+            raise Exception("Unknown error occured. Exiting...")
+
     def _push_to_git(self):
-        os.system('git commit {} master'.format(self._git_name))
-        os.system('git push {} master'.format(self._git_name))
-        os.system('git pull {} master'.format(self._git_name))
+        os.system('git commit {}'.format(self._git_name))
+        os.system('git push {}'.format(self._git_name))
+        os.system('git pull {}'.format(self._git_name))
 
-
-pts = PhoronixTestSuite()
+path = '~/Phoronix-Test-Suite'
+pts = PhoronixTestSuite(path)
 if sys.argv[1] == 'install':
     pts.install_tests()
 elif sys.argv[1] == 'run':

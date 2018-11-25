13504b77c78ccf044ebd0c23430d48dd0cdda145
Johnathan Becker <historybuffjb@gmail.com>
2018-11-25 08:29:44 -0700

    Update Code

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-def move_folder_contents(from_path, to_path):
-    shutil.copytree(from_path, to_path)
-from_path = r'C:\Users\John Becker\Dropbox\Documents\School Stuff\UC Denver New Degree'
-to_path = r'C:\Users\John Becker\Dropbox\Documents\School Stuff\UC Denver New Degree\Fall 2018\Operating Systems\Phoronix-Test-Suite\test'
-move_folder_contents(from_path, to_path)

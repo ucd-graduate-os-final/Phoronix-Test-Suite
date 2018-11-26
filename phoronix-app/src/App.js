4bb2896aeaac19ba7547a480b768f9235cb1d96c
Johnathan Becker <historybuffjb@gmail.com>
2018-11-26 13:38:50 -0700

    Update Code

--- phoronix-app/src/App.js
+++ phoronix-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SideBar from './sidebar';
+import ResultsParser from './results_parser.js'
 
 import './App.css';
 
@@ -8,12 +9,13 @@ export default function App() {
     <div id="App">
       <SideBar />
       <div id="header">
-        <h1>Whale of a good time. A look at virtual machine benchmarks</h1>
+        <h1>Whale of a good time. A look at Virtual Machine Performance</h1>
       </div>
       <div id="Body">
       
       </div>
       <div id="footer">
+      <p> Written by Tish Konz, Andrew Hill and John Becker </p>
       </div>
     </div>
   );
--- phoronix-app/src/sidebar.js
+++ phoronix-app/src/sidebar.js
@@ -3,13 +3,14 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 
+
 export default props => {
   return (
     <Menu>
       <a className="menu-item" href="/">Home</a>
-      <a className="menu-item" href="/laravel">Standard VM</a>
-      <a className="menu-item" href="/angular">Docker VM</a>
-      <a className="menu-item" href="/react">Nested VM</a>
+      <a className="menu-item" href="#">Standard VM</a>
+      <a className="menu-item" href="#">Docker VM</a>
+      <a className="menu-item" href="#">Nested VM</a>
     </Menu>
   );
 };
